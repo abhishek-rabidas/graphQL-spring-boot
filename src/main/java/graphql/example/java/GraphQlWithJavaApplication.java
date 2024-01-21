266d10eda0ba0bcd697ed23f8079545b4e13923c
abhishek-rabidas <avisec07@gmail.com>
2024-01-21 20:40:26 +0530

    project init

---
+++ src/main/java/graphql/example/java/GraphQlWithJavaApplication.java
@@ -0,0 +1,13 @@
+package graphql.example.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/graphql/example/java/GraphQlWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package graphql.example.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
