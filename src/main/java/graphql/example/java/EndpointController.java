27b1db972436baa3bcd25e0100e722982d6f92d2
abhishek-rabidas <avisec07@gmail.com>
2024-01-21 23:08:34 +0530

    response entity removed from api response

--- src/main/java/graphql/example/java/EndpointController.java
+++ src/main/java/graphql/example/java/EndpointController.java
@@ -1,12 +1,14 @@
 package graphql.example.java;
 
+import graphql.example.java.models.Record;
 import graphql.example.java.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class EndpointController {
@@ -15,22 +17,22 @@ public class EndpointController {
     private RequestResolverService service;
 
     @QueryMapping
-    public ResponseEntity<Object> getRecords() {
-        return ResponseEntity.ok(service.getRecords());
+    public List<Record> getRecords() {
+        return service.getRecords();
     }
 
     @QueryMapping
-    public ResponseEntity<Object> getUsers() {
-        return ResponseEntity.ok(service.getUsers());
+    public List<User> getUsers() {
+        return service.getUsers();
     }
 
     @MutationMapping
-    public ResponseEntity<Object> saveRecord(@Argument String details, @Argument Integer userId) {
-        return ResponseEntity.ok(service.saveRecord(details, userId));
+    public Record saveRecord(@Argument String details, @Argument Integer userId) {
+        return service.saveRecord(details, userId);
     }
 
     @MutationMapping
-    public ResponseEntity<User> addUser(@Argument String name, @Argument String address) {
-        return ResponseEntity.ok(service.saveUser(name, address));
+    public User addUser(@Argument String name, @Argument String address) {
+        return service.saveUser(name, address);
     }
 }
